43fe45fb0689cfa2d396b0df7a3b2b3ec8eeed24
Madhu097 <madhukuruva20@gmail.com>
2026-01-06 15:51:19 +0530

    chore: remove unnecessary documentation and debug files

--- api/index.js
+++
@@ -1,8 +0,0 @@
-// Vercel serverless function wrapper
-import('../dist/index.js').then(module => {
-    module.default;
-}).catch(err => {
-    console.error('Failed to load server:', err);
-});
-
-export { default } from '../dist/index.js';
